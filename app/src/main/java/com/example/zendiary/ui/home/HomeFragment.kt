5f0778cf136a708b6233896e5c5a3092481e2c3c
Dương Ngọc Quang Khiêm <128200532+KhimDuong@users.noreply.github.com>
2025-01-02 20:09:57 +0700

    Notification content

--- app/src/main/java/com/example/zendiary/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/zendiary/ui/home/HomeFragment.kt
@@ -90,7 +90,7 @@ class HomeFragment : Fragment() {
         val dialogView = LayoutInflater.from(requireContext()).inflate(R.layout.dialog_notification, null)
 
         // Reference the views inside the custom layout
-        val iconImageView: ImageView = dialogView.findViewById(R.id.iv_dialog_icon)
+        //val iconImageView: ImageView = dialogView.findViewById(R.id.iv_dialog_icon)
         val titleTextView: TextView = dialogView.findViewById(R.id.tv_dialog_title)
         val messageTextView: TextView = dialogView.findViewById(R.id.tv_dialog_message)
         val viewAllButton: Button = dialogView.findViewById(R.id.btn_dialog_view_all)
@@ -100,9 +100,9 @@ class HomeFragment : Fragment() {
         titleTextView.text = "Notification Alert"
         messageTextView.text = """
         Here are your latest updates:
-        1. New task assigned.
-        2. Upcoming meeting at 3 PM.
-        3. Your profile update was successful.
+        1. Upcoming meeting at 3 PM.
+        2. Upcoming meeting at 9 AM.
+        3. 3 Years anniversary
     """.trimIndent()
 
         // Build the dialog
